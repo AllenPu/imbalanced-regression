deae218e31e6b2a5124f7dd4e597286a64247afc
AllenPu <18651885620@163.com>
2022-06-06 20:30:34 -0400

    add resnet cls model

--- imdb-wiki-dir/resnet18.py
+++ imdb-wiki-dir/resnet18.py
@@ -13,3 +13,15 @@ def resent18_regression():
         nn.Linear(256, 1)
     )
     return model
+
+
+def resnet18_cls(num_cls = 100):
+    model = models.resnet18(pretrained=False)
+    fc_inputs = model.fc.in_features
+    model.fc = nn.Sequential(
+        nn.Linear(fc_inputs, 256),
+        nn.ReLU(),
+        nn.Dropout(),
+        nn.Linear(256, num_cls)
+    )
+    return model
